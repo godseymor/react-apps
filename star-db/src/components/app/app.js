87feb6e9f733bfe479ef65035806bef0e7cbfbd4
aleksandr.malets <malets@utip.ru>
2020-03-11 14:24:14 +0500

    8-5

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -4,49 +4,42 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ErrorButton from '../error-button';
 import PeoplePage from '../people-page';
-import ErrorBoundry from "../error-boundry";
+import ErrorBoundry from '../error-boundry';
 
 import './app.css';
 
 export default class App extends Component {
+    state = {
+        showRandomPlanet: true
+    };
 
-  state = {
-    showRandomPlanet: true
-  };
+    toggleRandomPlanet = () => {
+        this.setState(state => {
+            return {
+                showRandomPlanet: !state.showRandomPlanet
+            };
+        });
+    };
 
-  toggleRandomPlanet = () => {
-    this.setState((state) => {
-      return {
-        showRandomPlanet: !state.showRandomPlanet
-      }
-    });
-  };
+    render() {
+        const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
 
-  render() {
+        return (
+            <ErrorBoundry>
+                <div className='stardb-app'>
+                    <Header />
+                    {planet}
 
-    const planet = this.state.showRandomPlanet ?
-      <RandomPlanet/> :
-      null;
+                    <div className='row mb2 button-row'>
+                        <button className='toggle-planet btn btn-warning btn-lg' onClick={this.toggleRandomPlanet}>
+                            Toggle Random Planet
+                        </button>
+                        <ErrorButton />
+                    </div>
 
-    return (
-      <ErrorBoundry>
-        <div className="stardb-app">
-          <Header />
-          { planet }
-
-          <div className="row mb2 button-row">
-            <button
-              className="toggle-planet btn btn-warning btn-lg"
-              onClick={this.toggleRandomPlanet}>
-              Toggle Random Planet
-            </button>
-            <ErrorButton />
-          </div>
-
-          <PeoplePage />
-
-        </div>
-      </ErrorBoundry>
-    );
-  }
+                    <PeoplePage />
+                </div>
+            </ErrorBoundry>
+        );
+    }
 }
--- star-db/src/components/people-page/people-page.js
+++ star-db/src/components/people-page/people-page.js
@@ -9,39 +9,29 @@ import ErrorBoundry from '../error-boundry';
 import './people-page.css';
 
 export default class PeoplePage extends Component {
+    swapiService = new SwapiService();
 
-  swapiService = new SwapiService();
+    state = {
+        selectedPerson: 3
+    };
 
-  state = {
-    selectedPerson: 3
-  };
+    onPersonSelected = selectedPerson => {
+        this.setState({ selectedPerson });
+    };
 
-  onPersonSelected = (selectedPerson) => {
-    this.setState({ selectedPerson });
-  };
+    render() {
+        const itemList = (
+            <ItemList onItemSelected={this.onPersonSelected} getData={this.swapiService.getAllPeople}>
+                {i => `${i.name} (${i.birthYear})`}
+            </ItemList>
+        );
 
-  render() {
+        const personDetails = (
+            <ErrorBoundry>
+                <PersonDetails personId={this.state.selectedPerson} />
+            </ErrorBoundry>
+        );
 
-    const itemList = (
-      <ItemList
-        onItemSelected={this.onPersonSelected}
-        getData={this.swapiService.getAllPeople}>
-
-        {(i) => (
-          `${i.name} (${i.birthYear})`
-        )}
-
-      </ItemList>
-    );
-
-    const personDetails = (
-      <ErrorBoundry>
-        <PersonDetails personId={this.state.selectedPerson} />
-      </ErrorBoundry>
-    );
-
-    return (
-      <Row left={itemList} right={personDetails} />
-    );
-  }
+        return <Row left={itemList} right={personDetails} />;
+    }
 }
