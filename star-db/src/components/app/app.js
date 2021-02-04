05cebe50930b59666f35be869c0feb07169b6287
godSeymor <godseymor@gmail.com>
2021-02-04 10:52:15 +0500

    patterns complete

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Header from '../header';
-import RandomPlanet from '../random-planet';
-import ErrorButton from '../error-button';
-import PeoplePage from '../people-page';
-import ErrorBoundry from '../error-boundry';
+import Header from "../header";
+import RandomPlanet from "../random-planet";
+import ErrorBoundry from "../error-boundry";
 
-import './app.css';
+import Row from "../row/row";
+import ItemDetails, { Record } from "../item-details/item-details";
+import SwapiService from "../../services/swapi-service";
+
+import "./app.css";
 
 export default class App extends Component {
+    swapiService = new SwapiService();
+
     state = {
-        showRandomPlanet: true
+        showRandomPlanet: true,
     };
 
     toggleRandomPlanet = () => {
-        this.setState(state => {
+        this.setState((state) => {
             return {
-                showRandomPlanet: !state.showRandomPlanet
+                showRandomPlanet: !state.showRandomPlanet,
             };
         });
     };
@@ -24,20 +28,29 @@ export default class App extends Component {
     render() {
         const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
 
+        const { getPerson, getStarship, getPersonImage, getStarshipImage } = this.swapiService;
+
+        const personDetails = (
+            <ItemDetails itemId={11} getData={getPerson} getImageUrl={getPersonImage}>
+                <Record field="gender" label="Gender" />
+                <Record field="eyeColor" label="Eye Color" />
+            </ItemDetails>
+        );
+
+        const starshipDetails = (
+            <ItemDetails itemId={5} getData={getStarship} getImageUrl={getStarshipImage}>
+                <Record field="model" label="Model" />
+                <Record field="length" label="Length" />
+                <Record field="costInCredits" label="Cost" />
+            </ItemDetails>
+        );
+
         return (
             <ErrorBoundry>
-                <div className='stardb-app'>
+                <div className="stardb-app">
                     <Header />
-                    {planet}
 
-                    <div className='row mb2 button-row'>
-                        <button className='toggle-planet btn btn-warning btn-lg' onClick={this.toggleRandomPlanet}>
-                            Toggle Random Planet
-                        </button>
-                        <ErrorButton />
-                    </div>
-
-                    <PeoplePage />
+                    <Row left={personDetails} right={starshipDetails} />
                 </div>
             </ErrorBoundry>
         );
